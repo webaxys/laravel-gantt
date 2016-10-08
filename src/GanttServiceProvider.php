505c6234e4f702478d92969455b3fa6014d8d057
Scott Watkins <scott.watkins@mbible.org>
2016-10-08 18:53:55 -0500

    initial import

---
+++ src/GanttServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Swatkins\Gantt;
+
+use Illuminate\Support\ServiceProvider;
+
+class GanttServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/assets/css' => public_path('vendor/swatkins/gantt/css'),
+        ], 'gantt');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
